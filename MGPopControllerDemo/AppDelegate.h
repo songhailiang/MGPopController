8de30cd3c09cd823e27ab059d76cd687fddfca2d
songhailiang <jaysea094@126.com>
2016-08-04 15:22:13 +0800

    init

---
+++ MGPopControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MGPopControllerDemo
+//
+//  Created by 宋海梁 on 16/8/4.
+//  Copyright © 2016年 宋海梁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MGPopControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MGPopControllerDemo
+//
+//  Created by 宋海梁 on 16/8/4.
+//  Copyright © 2016年 宋海梁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
